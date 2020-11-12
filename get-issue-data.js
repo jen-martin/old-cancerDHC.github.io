9b9feb92211f2d57ceea2ab8770ccbe2d6174376
Jen Martin <martjen4@oregonstate.edu>
2020-11-12 11:31:58 -0800

    gets dates, completion status, dependencies from issues

--- get-issue-data.js
+++ get-issue-data.js
@@ -1,77 +1,220 @@
-// Searches the body of the GitHub Issue for
-// the main gantt chart elements. Each gantt chart
-// element is on its own line in the main issue body.
-function searchIssueBody(keywords, bodyText){
-	const lines = bodyText.split('\r\n');
-	var str = '';
-	lines.forEach((line) => {
-		if (line.includes(keywords)) {
-			str = line;
-			str = str.split(keywords)[1];
-			str = str.replace(/\s/g, '');
-		}
-	});
-	return str;
-}
-
 //Creates a unique ID for each GitHub issue.
 //Because we're gathering issues across multiple 
 //repos, the syntax of the id is:
 // [repo name] + 'Issue #' + [GitHub issue number]
-function getGanttID(repoName,issueNumber){
+function getGanttID(repoName, issueNumber){
 	var str = repoName + ' Issue #' + issueNumber;
 	return str;
 }
 
-//Gets the task start date from the GitHub issue
-//body. If it's missing from the body, create
-//a start date based on the issue's date of creation.
-//The start date should be entered in the GitHub main 
-//issue text on a signle line as: 'Start Date: YYYY-MM-DD'
-//TO DO - reformat the issue create date to YYYY-MM-DD.
-function getGanttStartDate(bodyText, createDate) {
-	var start = createDate; //default
-	//var start = '2020-01-01'; // default
-	var str = searchIssueBody('Start Date:', bodyText);
-	if (str != null && str != '') {start = str;}
-	return start;
+//Gets the task start or date from the GitHub issue
+//milestone. Milestone should contain the phase (e.g., 
+//"Phase 2") and quarter (e.g., "Quarter 2"). Note that 
+//each phase starts in quarter 2 (5/1) and ends in
+//quarter 1 of the next calendar year.
+// opt = 0 gets the start date; opt = 1 gets the end date
+function getGanttDate(milestone, opt) {
+	var date = '';
+	if (milestone != null) {
+		if (milestone.title != null) {
+			var str = milestone.title.toLowerCase();
+			if (str.search("phase 2") >= 0) {
+				if (opt == 0) {
+					date = '2020-05-01'; // default is start of Phase 2
+					if (str.search("quarter 2") >= 0) {
+						date = '2020-05-01';
+					} else if (str.search("quarter 3") >= 0) {
+						date = '2020-08-01';
+					} else if (str.search("quarter 4") >= 0) {
+						date = '2020-11-01';
+					} else if (str.search("quarter 1") >= 0) {
+						date = '2021-02-01';
+					}
+				} else if (opt == 1) {
+					date = '2021-04-30'; // default is end of Phase 2
+					if (str.search("quarter 2") >= 0) {
+						date = '2020-07-31';
+					} else if (str.search("quarter 3") >= 0) {
+						date = '2020-10-31';
+					} else if (str.search("quarter 4") >= 0) {
+						date = '2021-01-31';
+					} else if (str.search("quarter 1") >= 0) {
+						date = '2021-04-30';
+					}
+				}
+			} else if (str.search("phase 3") >= 0) {
+				if (opt == 0) {
+					date = '2021-05-01'; // default is start of Phase 3
+					if (str.search("quarter 2") >= 0) {
+						date = '2021-05-01';
+					} else if (str.search("quarter 3") >= 0) {
+						date = '2021-08-01';
+					} else if (str.search("quarter 4") >= 0) {
+						date = '2021-11-01';
+					} else if (str.search("quarter 1") >= 0) {
+						date = '2022-02-01';
+					}
+				} else if (opt == 1) {
+					date = '2022-04-30'; // default is end of Phase 3
+					if (str.search("quarter 2") >= 0) {
+						date = '2021-07-31';
+					} else if (str.search("quarter 3") >= 0) {
+						date = '2021-10-31';
+					} else if (str.search("quarter 4") >= 0) {
+						date = '2022-01-31';
+					} else if (str.search("quarter 1") >= 0) {
+						date = '2022-04-30';
+					}
+				}
+			}
+			return date;
+		}
+	}
 }
 
-//Gets the task end date from the GitHub issue
-//body. The end date should be entered in the GitHub main 
-//issue text on a single line as: 'End Date: YYYY-MM-DD'
-//TO DO - check that the end date is
-//later than the start date.
-function getGanttEndDate(bodyText) {
-	var end = '2020-12-31'; // default
-	var str = searchIssueBody('End Date:', bodyText);
-	if (str != null && str != '') {end = str;}
-	return end;
-}
-
-//Gets the task completion percentage from the GitHub issue
-//body. It should be entered in the GitHub main issue
-// text on a single line, e.g., 'Progress (0-1): .75'
-function getGanttProgress(bodyText) {
+//Calculates the task completion percentage
+function getGanttProgress(bodyText, status) {
 	var progress = 0; 
-	var str = searchIssueBody('Progress (0-1):', bodyText);
-	progress = str * 100;
+	if (status.search("closed") >= 0) {
+		progress = 100;
+	} else if (status.search("open") >= 0) {
+		var checked = 0;
+		var unchecked = 0;
+		var total = 0;
+		//count number of open check boxes
+		var patt = /- \[ \]/g;
+	    if (patt.test(bodyText)) {
+	    	var result1 = bodyText.match(patt);
+	    	unchecked = result1.length;
+	    }
+		//count number of closed check boxes
+		patt = /- \[x\]/g;
+	    if (patt.test(bodyText)) {
+	    	var result2 = bodyText.match(patt);
+	    	checked = result2.length;
+	    }
+		// calculate percent complete
+		total = checked + unchecked;
+		if (total > 0) {
+			progress = Math.round((checked / total) * 100);
+		}
+	}
 	return progress;
 }
 
 //Gets any task dependencies from the GitHub issue
-//body. It should be entered in the GitHub main issue
-// text on a single line, e.g., 'Dependencies: #5,#20'
-// (the numbers in the list of dependent items correspond 
-// to the GitHub issue numbers)
+//body. Dependencies should be included in the body
+//text using the hash symbol followed by the issue
+//number (e.g., #19, #8)
 function getGanttDependencies(bodyText, repoName) {
 	var dependencies = '';
-	var str = searchIssueBody('Dependencies:', bodyText);
-	if (str != null && str != '') {
-		dependencies = str.replace(/#/g, repoName + ' Issue #');
+	//check if body text contains reference to another issue
+	// (e.g., #19)
+	var patt = /#\d+/g;
+	if (patt.test(bodyText)) {
+		var result = bodyText.match(patt);
+		for(var i = 0; i < result.length; i++) {
+			// Note that the syntax of the issue names is:
+            // [repo name] + ' Issue # ' + [GitHub issue number]
+			dependencies = dependencies + repoName + " Issue " + result[i];
+			if (i < (result.length - 1)) {
+				dependencies = dependencies + ", ";
+			}
+		};
 	}
 	return dependencies;
 }
+
+//determine the item's level from the title
+//or label
+function getParent(title, labels) {
+	var parent = '';
+	//first check title
+	var str = title.trim().toLowerCase();
+	str = str.split(' ')[0]; //get first "word" in the title
+	var patt1 = /^\d[a-z]/; //starts with digit-letter
+	var patt2 = /^\d[a-z]\d/; //starts with digit-letter-digit
+	var patt3 = /^\d[a-z]\d[a-z]/; //starts with digit-letter-digit-letter
+	if (patt1.test(str)) {
+		//starts with digit-letter
+		if (patt3.test(str)) {
+			//starts with digit-letter-digit-letter
+			parent = str.substring(0, 3);
+		} else if (patt2.test(str)) {
+			//starts with digit-letter-digit
+			parent = str.substring(0, 2);
+		} else {
+			parent = str.substring(0, 1);
+		}
+	} else {
+		// TO DO - try label
+		
+	}
+	//console.log(title, labels, parent);
+	return parent;
+}
+
+
+//sort tasks
+function sortTasks(alltasks) {
+	var sortedTasks = [];
+
+	var phaseNames = ['Phase 1','Phase 2','Phase 3','Phase 4'];
+	var phaseCompleteNum = [];
+	var phaseCompleteDen = [];
+	var phaseProgress = [];
+	var firstLevel = ['2a','2b','2c'];
+	var secondLevel = ['2a1','2a2','2b1','2b2'];
+	var secondLevelProgress = [0,0,0,0];
+	var secondLevelNumerator = [0,0,0,0];
+	var secondLevelDenominator = [0,0,0,0];
+	var secondLevelHasChild = [0,0,0,0]; // TO DO - if the item doesn't have a child, use the item's percent complete as the progress
+
+	for (var j = 0; j < secondLevel.length; j++) {
+		var item = [];
+		//defaults
+		item.id = secondLevel[j];
+		item.name = secondLevel[j];
+		item.start = '2020-05-01';
+		item.end = '2021-04-30';
+		item.dependencies = '';
+		item.url = '';
+		item.custom_class = '';
+		for (let i in alltasks) {
+			//check to see if the item exits or needs to be created
+			// calculate progress level
+			var str = alltasks[i].parent;
+			if (str != '') { 
+				if (str == secondLevel[j]) {
+					secondLevelNumerator[j] = secondLevelNumerator[j] + alltasks[i].progress;
+					secondLevelDenominator[j] = secondLevelDenominator[j] + 100;
+					secondLevelProgress[j] = Math.round((secondLevelNumerator[j] / secondLevelDenominator[j]) * 100);
+					secondLevelHasChild[j] = secondLevelHasChild[j] + 1
+					console.log(secondLevelNumerator[j],secondLevelDenominator[j],secondLevelProgress[j]);				
+				}
+			}
+		}
+		item.progress = secondLevelProgress[j];
+		sortedTasks.push(item);
+	}
+	
+	for (let i in alltasks) {
+		item = [];
+		item.id = alltasks[i].id;
+		item.name = alltasks[i].name;
+		item.start = alltasks[i].start;
+		item.end = alltasks[i].end;
+		item.dependencies = alltasks[i].dependencies;
+		item.progress = alltasks[i].progress;
+		//the GitHub issue URL to link to in the pop-up
+		item.url = alltasks[i].url;
+		//set the color code for the progress bar
+		item.custom_class = alltasks[i].repo;
+		sortedTasks.push(item);
+	}
+	return sortedTasks;
+}
+
 
 //Write all GitHub issues in all repos to a TSV file.
 function writeGanttDataFile() {
@@ -109,12 +252,14 @@ function writeGanttDataFile() {
 					// Get the issue title
 					tsvRow += '\t\'' + data[i].title +'\'';
 					
-					// Get the issue body
-					// extract start date, end date, progress, dependencies
-					// from the issue body
-					tsvRow += '\t\'' + getGanttStartDate(data[i].body,data[i].created_at) +'\'';
-					tsvRow += '\t\'' + getGanttEndDate(data[i].body) +'\'';
-					tsvRow += '\t' + getGanttProgress(data[i].body);
+					// Determine the start and end dates from the issue milestone
+					tsvRow += '\t\'' + getGanttDate(data[i].milestone, 0) +'\''; //start date
+					tsvRow += '\t\'' + getGanttDate(data[i].milestone, 1) +'\''; //end date
+
+					// TO DO - Determine Progress completed
+					tsvRow += '\t' + getGanttProgress(data[i].body, data[i].state);
+
+					//Extract any dependencies from the issue body
 					tsvRow += '\t\'' + getGanttDependencies(data[i].body, repoName) +'\'';
 					// Log the repo url
 					tsvRow += '\t' + data[i].html_url + '\n';
@@ -136,8 +281,8 @@ function writeGanttDataFile() {
 // to display the information.
 // TO DO: add custom formatting of bars based on repo; see
 // https://github.com/frappe/gantt/issues/175 for implementation.
-function getRequest(repo, url, tasks, resolve, reject) {
-	url = url + repo + "/issues";
+function getRequest(repo, url, alltasks, resolve, reject) {
+	url = url + repo + "/issues?state=all&per_page=100";
 	console.log("Sending request to get data: " + url);
 	  var xhttp;
 	  if (window.XMLHttpRequest) {
@@ -157,16 +302,18 @@ function getRequest(repo, url, tasks, resolve, reject) {
 				{
 					 'id': getGanttID(repo, data[i].number),
 					 'name': data[i].title,
-					 'start': getGanttStartDate(data[i].body,data[i].created_at),
-					 'end': getGanttEndDate(data[i].body),
-					 'progress': getGanttProgress(data[i].body),
+					 'start': getGanttDate(data[i].milestone, 0),
+					 'end': getGanttDate(data[i].milestone, 1),
+					 'progress': getGanttProgress(data[i].body, data[i].state),
 					 'dependencies': getGanttDependencies(data[i].body,repo),
 					 //get the GitHub issue URL so we can link to it in the pop-up
 					 'url': data[i].html_url,
-					 //color code each repo's tasks
-					 'custom_class': repo
+					 //repo name
+					 'repo': repo,
+					 //parent item
+					 'parent': getParent(data[i].title, data[i].labels),
 				}];
-				tasks.push(item[0]); 
+				alltasks.push(item[0]); 
 			}
 		  
 		  return resolve();
@@ -186,28 +333,29 @@ function createTasks() {
 	//var repos = ["test-gantt", "jen-martin.github.io"]; //test
 		
 	//writeGanttDataFile();
-	var tasks = [];
+	var alltasks = [];
 
 	//sent each call to each repo asynchronously and wait for them all to finish
 	var promises = [];
 	for(var i = 0; i < repos.length; i++) {
-		var p = new Promise(function(resolve, reject){getRequest(repos[i], url, tasks, resolve, reject);});
+		var p = new Promise(function(resolve, reject){getRequest(repos[i], url, alltasks, resolve, reject);});
 		promises.push(p);
 	}
 	Promise.all(promises).then(function() {
 		console.log('all promises executed');
+		//console.log(alltasks);
+
+		var tasks = sortTasks(alltasks); 
+		console.log(tasks);
+
 		var gantt = new Gantt(".gantt-target", tasks, {
 			//create a custom pop-up with the task URL
 			custom_popup_html: function(task) {
-				//TO DO: format start/end dates nicely
-				//const start_date = task._start.format('MMM D');
-				//const end_date = task._end.format('MMM D');
 			  return `
 				<div class="details-container">
 				  <div class="title">${task.name}</div>
 				  <div class="subtitle">
-				  Start: ${task.start}<br />
-				  End: ${task.end}<br />
+				  Due: ${task.end} &nbsp;&nbsp;&nbsp;&nbsp; ${task.progress}% Complete<br />
 				  <a href=${task.url} target="_blank">${task.url}</a>
 				  </div>
 				</div>
